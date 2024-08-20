8961d20cfb39afedf68a0bae1201f1640da29761
EmilyTheFox <emilyborgers@outlook.com>
2024-08-20 19:15:42 +0200

    FIRE-3 Fire endpoint add validation
    Added form validation for the request

--- app/Http/Requests/Fire/FireRequest.php
+++ app/Http/Requests/Fire/FireRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Fire;
 
 use App\Http\Requests\APIFormRequest;
+use App\Rules\IsTaxSystemRule;
+use App\Rules\RequiresIncreaseAmountRule;
 
 class FireRequest extends APIFormRequest
 {
@@ -20,8 +22,32 @@ class FireRequest extends APIFormRequest
     public function rules(): array
     {
         return [
-            'age'  => 'required|integer|min:0|max:119',
-            'endAge' => 'required|integer|min:1|max:120',
+            'startAge'        => 'required|integer|min:0|max:119',
+            'endAge'          => 'required|integer|min:1|max:120',
+            'realInflation'   => 'required|boolean',
+            'staticInflation' => 'required_if:realInflation,false|decimal:0,2|min:0|max:10',
+            'flatReturns'     => 'decimal:0,2|min:0.1|max:50',
+            'taxSystem'       => ['required', 'string', 'bail', new IsTaxSystemRule(['allowNone' => true])],
+            'dataSince'       => 'required|integer|min:1871',
+            'startBalance'    => 'required|integer|min:0|max_digits:15',
+
+            'contributions' => 'required|array|min:1',
+
+            'contributions.*.startAge'          => 'required|integer|min:0|max:119',
+            'contributions.*.endAge'            => 'required_unless:contributions.*.frequency,One-Off|integer|min:1|max:120|exclude_if:contributions.*.frequency,One-Off',
+            'contributions.*.amount'            => 'required|decimal:0,2|min:1|max:1000000000000', // max 1 trillion because max_digits: doesnt work with decimals
+            'contributions.*.frequency'         => 'required|string|in:One-Off,Monthly,Quarterly,Yearly',
+            'contributions.*.increaseFrequency' => 'required_unless:contributions.*.frequency,One-Off|string|in:Never,Monthly,Quarterly,Yearly,Match Inflation|exclude_if:contributions.*.frequency,One-Off',
+            'contributions.*.increaseAmount'    => [new RequiresIncreaseAmountRule(), 'exclude_if:contributions.*.frequency,One-Off', 'exclude_if:contributions.*.increaseFrequency,Never', 'exclude_if:contributions.*.increaseFrequency,Match Inflation'],
+
+            'withdrawals' => 'present|array|min:1',
+
+            'withdrawals.*.startAge'          => 'required|integer|min:0|max:119',
+            'withdrawals.*.endAge'            => 'required_unless:withdrawals.*.frequency,One-Off|integer|min:1|max:120|exclude_if:withdrawals.*.frequency,One-Off',
+            'withdrawals.*.amount'            => 'required|decimal:0,2|min:1|max:1000000000000', // max 1 trillion because max_digits: doesnt work with decimals
+            'withdrawals.*.frequency'         => 'required|string|in:One-Off,Monthly,Quarterly,Yearly',
+            'withdrawals.*.increaseFrequency' => 'required_unless:withdrawals.*.frequency,One-Off|string|in:Never,Monthly,Quarterly,Yearly,Match Inflation|exclude_if:withdrawals.*.frequency,One-Off',
+            'withdrawals.*.increaseAmount'    => [new RequiresIncreaseAmountRule(), 'exclude_if:withdrawals.*.frequency,One-Off', 'exclude_if:withdrawals.*.increaseFrequency,Never', 'exclude_if:withdrawals.*.increaseFrequency,Match Inflation']
         ];
     }
 
@@ -33,8 +59,12 @@ class FireRequest extends APIFormRequest
     public function messages(): array
     {
         return [
-            // 'age.required' => 'An age is required',
-            // 'endAge.required' => 'An end age is required',
+            'contributions.required' => 'Contributions is required and must have at least 1 item',
+            'contributions.*.frequency' => 'frequency is required and must be one of: One-Off, Monthly, Quarterly or Yearly',
+            'contributions.*.increaseFrequency' => 'contributions.:index.increaseFrequency is required if contributions.:index.frequency isn\'t One-Off and must be one of: Never, Monthly, Quarterly, Yearly or Match Inflation',
+
+            'withdrawals.*.frequency' => 'frequency is required and must be one of: One-Off, Monthly, Quarterly or Yearly',
+            'withdrawals.*.increaseFrequency' => 'withdrawals.:index.increaseFrequency is required if withdrawals.:index.frequency isn\'t One-Off and must be one of: Never, Monthly, Quarterly, Yearly or Match Inflation'
         ];
     }
 }
